f1a5f9389236a3ed6c645831cffa37046fc1530c
TonyKess <tonykess@gmail.com>
2026-02-13 19:20:27 +0000

    removed some stuff

--- joblauncher.eccodna.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Loop through every config file in the configs directory
-for CONFIG_PATH in configs/project_*.config; do
-    echo "Submitting job for $CONFIG_PATH..."
-    
-    # Use --export to pass the specific path to the sbatch environment
-    sbatch --export=ALL,CONFIG_FILE="$CONFIG_PATH" submit.ednanf.customconfig.sh
-done
